3374ab237fc597b16642d4b6871b5257a598072d
mattdparker <matthewdanielparker@gmail.com>
2017-06-23 14:59:06 +0100

    changing ready for deployment

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 basedir = os.path.dirname(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + basedir + '/resources/uv.db'
-#SQLALCHEMY_DATABASE_URI = 'sqlite:////resources/uv.db'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + basedir + '/resources/uv.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////resources/uv.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'resources')
-#SQLALCHEMY_MIGRATE_REPO = '/resources'
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'resources')
+SQLALCHEMY_MIGRATE_REPO = '/resources'
